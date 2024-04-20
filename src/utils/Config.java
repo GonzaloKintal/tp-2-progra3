a2fee7da0728eb42a425794a80ec06232bdaadcb
Santiago Zarate <santiago2001zarate@gmail.com>
2024-04-19 23:06:25 -0300

    add config

---
+++ src/utils/Config.java
@@ -0,0 +1,32 @@
+package utils;
+
+import java.util.ArrayList;
+
+import org.openstreetmap.gui.jmapviewer.Coordinate;
+
+import logica.Provincia;
+
+public class Config {
+	public Provincia[] provincias;
+	
+	public Config() {
+		this.provincias = new Provincia[23];
+		
+		ArrayList<String> limitrofes_bsas = new ArrayList<>();
+		limitrofes_bsas.add("Cordoba");
+		limitrofes_bsas.add("Santa Fe");
+		limitrofes_bsas.add("Entre Rios");
+		limitrofes_bsas.add("La Pampa");
+		limitrofes_bsas.add("Rio Negro");
+		limitrofes_bsas.add("Ciudad Autonom de Buenos Aires");
+		provincias[0] = new Provincia("Buenos Aires", new Coordinate(-36.410246, -60.441624), limitrofes_bsas);
+		
+		ArrayList<String> limitrofes_cordoba = new ArrayList<>();
+		limitrofes_bsas.add("Buenos Aires");
+		limitrofes_bsas.add("Santa Fe");
+		limitrofes_bsas.add("San Luis");
+		limitrofes_bsas.add("La Pampa");
+		limitrofes_bsas.add("Santiago Del Estero");
+		provincias[1] = new Provincia("Córdoba", new Coordinate(-31.407339, -64.191297), limitrofes_cordoba);
+	}
+}
