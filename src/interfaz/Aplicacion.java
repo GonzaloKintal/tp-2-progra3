4ac2ba16fc8e084f5f1f1a322fc401c964842176
GonzaloKintal <kintalgonzalo40@gmail.com>
2024-04-19 21:02:49 -0300

    Arrancando el proyecto

---
+++ src/interfaz/Aplicacion.java
@@ -0,0 +1,80 @@
+package interfaz;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JSplitPane;
+
+import org.openstreetmap.gui.jmapviewer.Coordinate;
+import org.openstreetmap.gui.jmapviewer.JMapViewer;
+import org.openstreetmap.gui.jmapviewer.MapMarkerDot;
+import org.openstreetmap.gui.jmapviewer.interfaces.MapMarker;
+
+public class Aplicacion {
+
+	private JFrame frame;
+
+	private JMapViewer mapa;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Aplicacion window = new Aplicacion();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Aplicacion() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(300, 100, 600, 600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("JMapViewer");
+
+		JPanel panelMapa = new JPanel();
+		panelMapa.setLayout(null);
+
+		mapa = new JMapViewer();
+		mapa.setBounds(0, 0, 300, 600);
+		mapa.setZoomControlsVisible(false);
+
+		Coordinate posicion = new Coordinate(-42.3944, -64.425);
+		mapa.setDisplayPosition(posicion, 4);
+
+		MapMarker cordoba = new MapMarkerDot("", new Coordinate(-31.407339, -64.191297));
+		cordoba.getStyle().setBackColor(Color.RED);
+		mapa.addMapMarker(cordoba);
+
+		JPanel panelIzquierdo = new JPanel();
+		panelIzquierdo.setBackground(Color.WHITE);
+		panelIzquierdo.setPreferredSize(new Dimension(300, 600));
+
+		JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, panelIzquierdo, panelMapa);
+		splitPane.setResizeWeight(0);
+		splitPane.setDividerSize(0);
+
+		panelMapa.add(mapa);
+		frame.getContentPane().add(splitPane);
+	}
+
+}
